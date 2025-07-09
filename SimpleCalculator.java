c9e7fc47163f4c2bec96052ac579412922753d2d
shruti9527 <129036886+shruti9527@users.noreply.github.com>
2025-07-09 12:56:52 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,26 +1,19 @@
 public class SimpleCalculator {
-    public static void main(String[] args) {
-        double num1 = 20;
-        double num2 = 5;
-
-        // Addition
-        double sum = num1 + num2;
-        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
-
-        // Subtraction
-        double difference = num1 - num2;
-        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
-
-        // Multiplication
-        double product = num1 * num2;
-        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + product);
-
-        // Division
-        if (num2 != 0) {
-            double quotient = num1 / num2;
-            System.out.println("Division: " + num1 + " / " + num2 + " = " + quotient);
-        } else {
-            System.out.println("Division by zero is not allowed.");
-        }
+ 
+    public double add(double a, double b) {
+        return a + b;
+    }
+ 
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+ 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+ 
+    public double divide(double a, double b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return a / b;
     }
 }
